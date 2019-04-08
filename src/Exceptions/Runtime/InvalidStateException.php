28e099dea09a8f4cb857a956e771c6639ac4d5df
vody105 <filipsuska@gmail.com>
2019-04-08 13:35:59 +0200

    Debug: display query source files

---
+++ src/Exceptions/Runtime/InvalidStateException.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+namespace Nettrine\DBAL\Exceptions\Runtime;
+
+final class InvalidStateException extends RuntimeException
+{
+
+}
---
+++ src/Exceptions/Runtime/RuntimeException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace Nettrine\DBAL\Exceptions\Runtime;
+
+use RuntimeException as BasicRuntimeException;
+
+abstract class RuntimeException extends BasicRuntimeException
+{
+
+}
