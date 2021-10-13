080be6ae52564077c31ffdb31ca82783bda6ea70
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2021-10-13 08:35:36 +0200

    Codestyle

--- src/DI/DbalExtension.php
+++ src/DI/DbalExtension.php
@@ -152,10 +152,10 @@ final class DbalExtension extends CompilerExtension
 		$connectionDef = $builder->addDefinition($this->prefix('connection'))
 			->setFactory($config['wrapperClass'] ?? Connection::class)
 			->setFactory('@' . $this->prefix('connectionFactory') . '::createConnection', [
-			$config,
-			'@' . $this->prefix('configuration'),
-			$builder->getDefinitionByType(EventManager::class),
-		]);
+				$config,
+				'@' . $this->prefix('configuration'),
+				$builder->getDefinitionByType(EventManager::class),
+			]);
 
 		$debugConfig = $this->config->debug;
 		if ($debugConfig->panel) {
