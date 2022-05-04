f06af483912a6b307e76432fae92f6681d5c939b
Rixafy <rixafy@gmail.com>
2022-05-04 13:34:26 +0200

    Remove tracy from ProfilerLoggerTest

--- tests/cases/E2E/ProfilerLoggerTest.php
+++ tests/cases/E2E/ProfilerLoggerTest.php
@@ -14,7 +14,6 @@ use Ninjify\Nunjuck\Toolkit;
 use Tester\Assert;
 use Tests\Toolkit\DoctrineDeprecations;
 use Tests\Toolkit\NeonLoader;
-use Tracy\Bridges\Nette\TracyExtension;
 
 require_once __DIR__ . '/../../bootstrap.php';
 
@@ -28,7 +27,6 @@ Toolkit::test(function (): void {
 
 	$loader = new ContainerLoader(TEMP_DIR, true);
 	$class = $loader->load(function (Compiler $compiler): void {
-		$compiler->addExtension('tracy', new TracyExtension());
 		$compiler->addExtension('cache', new CacheExtension());
 		$compiler->addExtension('dbal', new DbalExtension());
 		$compiler->addConfig(NeonLoader::load('
