ac91979983c767790f15c2a67d4cf2937a23ab93
Milan Felix Šulc <rkfelix@gmail.com>
2023-06-11 17:17:45 +0200

    ConnectionFactory: simplified

--- src/ConnectionFactory.php
+++ src/ConnectionFactory.php
@@ -1,42 +1,25 @@
 <?php declare(strict_types = 1);
 
-/*
- * This file is part of the Doctrine Bundle
- *
- * The code was originally distributed inside the Symfony framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- * (c) Doctrine Project, Benjamin Eberlei <kontakt@beberlei.de>
- */
-
 namespace Nettrine\DBAL;
 
-use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
-use Doctrine\DBAL\Exception;
-use Doctrine\DBAL\Exception\DriverException;
-use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Type;
 
-/**
- * Connection
- */
 class ConnectionFactory
 {
 
 	/** @var mixed[] */
-	private $typesConfig = [];
+	private array $typesConfig = [];
 
 	/** @var mixed[] */
-	private $typesMapping = [];
+	private array $typesMapping = [];
 
 	/** @var mixed[] */
-	private $commentedTypes = [];
+	private array $commentedTypes = [];
 
-	/** @var bool */
-	private $initialized = false;
+	private bool $initialized = false;
 
 	/**
 	 * @param mixed[] $typesConfig
@@ -49,66 +32,26 @@ class ConnectionFactory
 	}
 
 	/**
-	 * Create a connection by name.
-	 *
 	 * @param mixed[] $params
 	 */
-	public function createConnection(
-		array $params,
-		?Configuration $config = null,
-		?EventManager $eventManager = null
-	): Connection
+	public function createConnection(array $params, ?Configuration $config = null): Connection
 	{
 		if (!$this->initialized) {
 			$this->initializeTypes();
 		}
 
-		$connection = DriverManager::getConnection($params, $config, $eventManager);
+		$connection = DriverManager::getConnection($params, $config);
+		$platform = $connection->getDatabasePlatform();
 
-		if (!empty($this->typesMapping)) {
-			$platform = $this->getDatabasePlatform($connection);
-			foreach ($this->typesMapping as $dbType => $doctrineType) {
-				$platform->registerDoctrineTypeMapping((string) $dbType, $doctrineType);
-			}
+		foreach ($this->typesMapping as $dbType => $doctrineType) {
+			$platform->registerDoctrineTypeMapping((string) $dbType, $doctrineType);
 		}
 
-		if (!empty($this->commentedTypes)) {
-			$platform = $this->getDatabasePlatform($connection);
-			foreach ($this->commentedTypes as $type) {
-				$platform->markDoctrineTypeCommented(Type::getType($type));
-			}
+		foreach ($this->commentedTypes as $type) {
+			$platform->markDoctrineTypeCommented(Type::getType($type));
 		}
 
 		return $connection;
-	}
-
-	/**
-	 * Try to get the database platform.
-	 *
-	 * This could fail if types should be registered to an predefined/unused connection
-	 * and the platform version is unknown.
-	 * For details have a look at DoctrineBundle issue #673.
-	 *
-	 * @throws Exception
-	 */
-	private function getDatabasePlatform(Connection $connection): AbstractPlatform
-	{
-		try {
-			return $connection->getDatabasePlatform();
-		} catch (Exception $driverException) {
-			if ($driverException instanceof DriverException) {
-				throw new Exception(
-					'An exception occurred while establishing a connection to figure out your platform version.' . PHP_EOL .
-					'You can circumvent this by setting a \'server_version\' configuration value' . PHP_EOL . PHP_EOL .
-					'For further information have a look at:' . PHP_EOL .
-					'https://github.com/doctrine/DoctrineBundle/issues/673',
-					0,
-					$driverException
-				);
-			}
-
-			throw $driverException;
-		}
 	}
 
 	private function initializeTypes(): void
